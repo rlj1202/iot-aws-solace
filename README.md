7fda524abab5f868b9979d73b805d7ae4b704856
Jisu Sim <rlj1202@gmail.com>
2022-10-18 05:17:21 +0100

    Added aws, camera

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Assignment 2
 
+5조
 - 심지수
 - 이연우
 
 ```
 npm i
 npm run start
+```
+
+.env:
+```
+SOLACE_MQTT_HOST_URL=<solace mqtt secure host url>
+SOLACE_USERNAME=<solace username>
+SOLACE_PASSWORD=<solace password>
+MQTT_CLIENT_ID=<client id what you want>
 ```
--- src/main.js
+++ src/main.js
@@ -16,7 +16,9 @@ if (result.error) {
 
 import MqttClient from './mqtt-client'
 
-var rpio = require('rpio');
+import { mqtt, http, iot } from 'aws-iot-device-sdk-v2'
+
+const rpio = require('rpio');
 
 async function main() {
     rpio.spiBegin();
@@ -30,13 +32,47 @@ async function main() {
 
     console.log("=== Starting MQTT producer ===");
 
-    let mqttClient;
+    let mqttClientSolace;
+    let mqttClientAWS;
+    let mqttClientAWSConnection;
 
     try {
-        mqttClient = MqttClient(mqttClientConfig);
+        mqttClientSolace = MqttClient(mqttClientConfig);
         console.log("Connecting MQTT client to Solace...");
-        await mqttClient.connect();
+        await mqttClientSolace.connect();
         console.log("MQTT client connected to Solace.");
+    } catch (err) {
+        console.error(err);
+        process.exit();
+    }
+
+    try {
+        let config_builder = iot.AwsIotMqttConnectionConfigBuilder
+            .new_mtls_builder_from_path(
+                `${__dirname}/../credentials/63fdd4ca9f62a048adced6dbc2616b9d5d3ed41b7d54429da6c6f879d232ba4f-certificate.pem.crt`,
+                `${__dirname}/../credentials/63fdd4ca9f62a048adced6dbc2616b9d5d3ed41b7d54429da6c6f879d232ba4f-private.pem.key`
+            );
+        config_builder.with_certificate_authority_from_path(
+            undefined,
+            `${__dirname}/../credentials/AmazonRootCA1.crt`
+        );
+
+        config_builder.with_clean_session(false);
+        config_builder.with_client_id('IoT_System_Client');
+        config_builder.with_endpoint('a2q6vmsmhirxv-ats.iot.ap-northeast-1.amazonaws.com');
+
+        const config = config_builder.build();
+
+        mqttClientAWS = new mqtt.MqttClient();
+        mqttClientAWSConnection = mqttClientAWS.new_connection(config);
+
+        console.log('Connecting to AWS client...')
+        await mqttClientAWSConnection.connect();
+        console.log('Connected');
+
+        await mqttClientAWSConnection.subscribe('pi/photo', mqtt.QoS.AtLeastOnce, () => {
+
+        });
     } catch (err) {
         console.error(err);
         process.exit();
@@ -56,7 +92,7 @@ async function main() {
         let dataJson = JSON.stringify(data);
 
         console.log(`Illuminance: ${dataJson}`);
-        mqttClient.send(`${mqttClientConfig.clientId}/ILLUMINANCE`, dataJson);
+        mqttClientSolace.send(`${mqttClientConfig.clientId}/ILLUMINANCE`, dataJson);
         
         await new Promise((res) => setTimeout(res, 1000));
     }
